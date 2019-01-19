44630b831249ba2b6e6c095f8664ec44904f8558
Qikai Xie <qikai.xie@enflame-tech.com>
2019-01-19 11:23:49 +0800

    delete visitor

--- ch29_visitor/visitor/man.cc
+++
@@ -1,8 +0,0 @@
-
-#include "visitor/man.h"
-#include "visitor/visitor_base.h"
-
-void Man::Accept(VisitorBase* state) {
-  // std::cout << "man accepting..." << std::endl;
-  state->GetManConclusion(this);
-}
--- ch29_visitor/visitor/woman.cc
+++
@@ -1,7 +0,0 @@
-#include "visitor/woman.h"
-#include "visitor/visitor_base.h"
-
-void Woman::Accept(VisitorBase* state) {
-  // std::cout << "woman accepting..." << std::endl;
-  state->GetWomanConclusion(this);
-}
